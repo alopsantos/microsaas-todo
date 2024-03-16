944c261149139eb6235e41d6efb98b7108b8ffd2
alopsantos <anderson_lops@hotmail.com>
2024-03-16 00:31:51 -0300

    :hammer: Update icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+import { Logo } from "@/components/Logo";
+import { Button } from "@/components/ui/button";
+import { Suspense } from "react";
+
 export default function Home() {
-  return <main>landingpage</main>;
+  return (
+    <div className="relative z-10 mt-[calc(30vh)] h-fit w-full max-w-md overflow-hidden border-y border-gray-200 sm:rounded-2xl sm:border sm:shadow-xl">
+      <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
+        <a href={"/"}>
+          <Logo className="h-10 w-10" />
+        </a>
+        <h3 className="text-xl font-semibold">
+          Sign in to {process.env.NEXT_PUBLIC_APP_NAME}
+        </h3>
+        <p className="text-sm text-gray-500">
+          Start creating short links with superpowers
+        </p>
+      </div>
+      <div className="flex flex-col space-y-3 bg-gray-50 px-4 py-8 sm:px-16">
+        <Suspense
+          fallback={
+            <>
+              <Button disabled={true} variant="secondary" />
+              <Button disabled={true} variant="secondary" />
+              <Button disabled={true} variant="secondary" />
+              <div className="mx-auto h-5 w-3/4 rounded-lg bg-gray-100" />
+            </>
+          }
+        ></Suspense>
+      </div>
+    </div>
+  );
 }
